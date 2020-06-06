c869e0e573aa3c8cbdcb781c23930fda4d955d75
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-06 16:26:51 +0300

    add Post and Profile serializer

---
+++ rating/serializers.py
@@ -0,0 +1,12 @@
+from .models import Post,Profile
+from rest_framework import serializers
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title','image','description','link')
+        
+class ProfileSeralizer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (' first_name','last_name',' email',' bio',' profile_pic')
